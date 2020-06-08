2d6c17011ee90f00c0fbd950975e99bd4f66c165
ben-reynolds <ben.reynolds@workday.com>
2020-06-08 11:52:21 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { Component } from 'react'
+import InputTransaction from './Components/InputTransaction';
+
+import {Container, Row, Col} from 'react-bootstrap'
+import TransactionsGrid from './Components/TransactionsGrid';
+import Result from './Components/Result';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+       transactions: []
+    }
+
+    this.addTransaction = this.addTransaction.bind(this)
+  }
+
+  addTransaction(transaction) {
+    let newTransactionsState = this.state.transactions
+    newTransactionsState.push(transaction)
+    this.setState({
+      transactions: newTransactionsState
+    })
+  }
+
+  render() {
+    const {transactions} = this.state
+    return (
+      <Container>
+        <Row className=" justify-content-md-center">
+          <InputTransaction transactions={transactions} addTransaction={this.addTransaction} />
+          <TransactionsGrid transactions={transactions} />
+          <Result transactions={transactions} />
+        </Row>
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default App
+
